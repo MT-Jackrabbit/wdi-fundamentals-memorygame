307d3c46ee2880a21d7dc9780b01bb0072d53e6c
Jeffrey Jackson <mt_jackrabbit@Jeffreys-MBP.attlocal.net>
2020-04-19 16:57:27 -0500

    added logic to randomize cards and add score board

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,3 +1,4 @@
+//this array holds the card objects and is constant, the cards never change
 const cards = [
 {
 	rank: "queen",
@@ -17,12 +18,22 @@ const cards = [
 {
 	rank: "king",
 	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
+	cardImage: "images/king-of-diamonds.png"
 }
 ];
 
-const cardsInPlay = [];
+let cardPosition = []; //this array holds the randomized cards and is what is displayed to the user
+let cardsInPlay = []; //this array holds the two cards that were selected by the user
+let gamesPlayed = 0;
+let gamesWon = 0;
 
+//adds a 'click' listener to the reset button
+document.getElementById('btnReset').addEventListener('click', resetBoard);
+
+/******************************************
+* Initalizes the game board when the page
+* is first loaded.
+*******************************************/
 function createBoard(){
 	for(let i = 0; i < cards.length; i++)
 	{
@@ -34,13 +45,25 @@ function createBoard(){
 	}
 }
 
+/******************************************
+* Called when the user clicks on one of the
+* cards.  This 'flips' the card over so that
+* the user can see what the face value is.
+* If two cards have been chosen then it calls
+* checkForMatch() to see if the user guessed
+* correctly.
+*******************************************/
 function flipCard(){
+	//retrieve the card id
 	let cardId = this.getAttribute('data-id');
-	this.setAttribute('src', cards[cardId].cardImage);
 
-	cardsInPlay.push(cards[cardId].rank);
+	//show the face of the card instead of the back
+	this.setAttribute('src', cardPosition[cardId].cardImage);
 
-	console.log("User flipped " + cards[cardId].rank + "\n" + cards[cardId].cardImage + "\n" + cards[cardId].suit);
+	//add the card to the cardsInPlay[]
+	cardsInPlay.push(cardPosition[cardId].rank);
+
+	//console.log("User flipped " + cardPosition[cardId].rank + "\n" + cardPosition[cardId].cardImage + "\n" + cardPosition[cardId].suit);
 
 	if(cardsInPlay.length === 2)
 	{
@@ -49,13 +72,69 @@ function flipCard(){
 
 }
 
+/*********************************************
+* Compares the two cards that were chosen
+* and lets the user know if they were correct
+* in the choice.
+**********************************************/
 function checkForMatch(){
 	if(cardsInPlay[0] === cardsInPlay[1])
 	{
-		alert("You found a match!");
+		gamesWon++;
+		document.getElementById('games-won').textContent = gamesWon;
+		alert("You found a MATCH!");
 	}
 	else
-		alert("Sorry try again!");
+		alert("Sorry NOT A MATCH try again!");
+
+	gamesPlayed++;
+	document.getElementById('games-played').textContent = gamesPlayed;
+}
+
+/******************************************
+* When called it resets the card images
+* so that the backs are showing and
+* clears the cardsInPlay[] so that
+* there is nothing in it.
+*******************************************/
+function resetBoard(){
+	const cardElement = document.getElementsByTagName('img');
+	//console.log(cardElement.length);
+
+	for(let i = 0; i < cardElement.length; i++)
+	{
+		cardElement[i].setAttribute('src', 'images/back.png');
+	}
+
+	//clear the cardsInPlay[] so player can start over
+	cardsInPlay = [];
+	randomizeBoard(4); //randomize the board if the user wants to play again
+}
+
+/******************************************
+* This function randomizes the cards so that
+* a different game is presented to user each
+* time the game is played.
+*******************************************/
+function randomizeBoard(numCards){
+	let randomNum = 0;
+	let cardsUsed = 0;
+	let usedNums = [0, 0, 0, 0];
+	cardPosition = [];
+
+	while(cardPosition.length < numCards)
+	{
+		randomNum = Math.floor((Math.random() * numCards));
+
+		if(usedNums[randomNum] === 0)
+		{
+			usedNums[randomNum] = 1;
+			cardPosition.push(cards[randomNum]);
+			cardsUsed++;
+			//console.log(cardPosition[cardPosition.length-1].suit + " : " + randomNum);
+		}
+	}
 }
 
 createBoard();
+randomizeBoard(4); //randomize the board on load
